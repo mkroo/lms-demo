88b1c70cea0b169ada0f95d28c71c0eb54de8777
ansrl0107 <ansrl0107@gmail.com>
2024-11-25 21:32:58 +0900

    feat: UserDetailsService 등록

--- src/main/kotlin/com/mkroo/lmsdemo/config/SecurityConfig.kt
+++ src/main/kotlin/com/mkroo/lmsdemo/config/SecurityConfig.kt
@@ -1,16 +1,19 @@
 package com.mkroo.lmsdemo.config
 
+import com.mkroo.lmsdemo.application.CustomUserDetailsService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
     @Bean
-    fun filterChain(http: HttpSecurity) : SecurityFilterChain {
+    fun filterChain(http: HttpSecurity, customUserDetailsService: CustomUserDetailsService) : SecurityFilterChain {
         return http
             .authorizeHttpRequests {
                 it
@@ -19,6 +22,7 @@ class SecurityConfig {
             }
             .formLogin(FormLoginConfigurer<HttpSecurity>::disable)
             .csrf(CsrfConfigurer<HttpSecurity>::disable)
+            .userDetailsService(customUserDetailsService)
             .build()
     }
 }
