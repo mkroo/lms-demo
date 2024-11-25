d3a6e08a084f718129036ec1c4c59c12275b802f
ansrl0107 <ansrl0107@gmail.com>
2024-11-25 19:20:28 +0900

    feat: 회원가입 요청 DTO

---
+++ src/main/kotlin/com/mkroo/lmsdemo/dto/RegisterUserRequest.kt
@@ -0,0 +1,23 @@
+package com.mkroo.lmsdemo.dto
+
+import com.mkroo.lmsdemo.domain.UserType
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
+data class RegisterUserRequest(
+    @field:NotBlank(message = "Name is required")
+    val name: String,
+
+    @field:NotBlank(message = "Email is required")
+    @field:Email(message = "Invalid email format")
+    val email: String,
+
+    @field:NotBlank(message = "Phone number is required")
+    val phoneNumber: String,
+
+    @field:NotBlank(message = "Password is required")
+    val password: String,
+
+    @field:NotBlank(message = "User type is required")
+    val userType: UserType
+)
