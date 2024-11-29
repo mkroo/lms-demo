4ab57829134ef9a1d7daa1c72d5c10aefadbcdf2
ansrl0107 <ansrl0107@gmail.com>
2024-11-29 14:50:07 +0900

    chore: dependency 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,8 @@ extra["snippetsDir"] = file("build/generated-snippets")
 
 dependencies {
     val kotestVersion = "5.9.1"
-    val kotestExtensionSpringVersion = "1.3.0"
     val jjwtVersion = "0.11.2"
+    val kotlinJdslVersion = "3.5.3"
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -52,17 +52,18 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:${kotestVersion}")
     testImplementation("io.kotest:kotest-assertions-core:${kotestVersion}")
     testImplementation("io.kotest:kotest-property:${kotestVersion}")
-    testImplementation("io.mockk:mockk:1.13.12")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring:${kotestExtensionSpringVersion}")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+
     testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.0.25")
+    testImplementation("io.mockk:mockk:1.13.12")
 
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
 
-    implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.5.3")
-    implementation("com.linecorp.kotlin-jdsl:jpql-render:3.5.3")
-    implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:3.5.3")
+    implementation("com.linecorp.kotlin-jdsl:jpql-dsl:$kotlinJdslVersion")
+    implementation("com.linecorp.kotlin-jdsl:jpql-render:$kotlinJdslVersion")
+    implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:$kotlinJdslVersion")
 
     implementation("org.redisson:redisson:3.38.1")
 }
