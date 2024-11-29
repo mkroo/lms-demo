62c8af63d2893b39bd49302900f6600335814434
ansrl0107 <ansrl0107@gmail.com>
2024-11-29 16:56:53 +0900

    fix: Json 요청 오류 처리

--- src/main/kotlin/com/mkroo/lmsdemo/api/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/mkroo/lmsdemo/api/GlobalExceptionHandler.kt
@@ -4,6 +4,7 @@ import com.mkroo.lmsdemo.dto.RestApiErrorResponse
 import com.mkroo.lmsdemo.dto.RestApiResponse
 import com.mkroo.lmsdemo.exception.*
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -26,5 +27,11 @@ class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     fun handleIllegalAuthenticationException(e: IllegalAuthenticationException): RestApiErrorResponse {
         return RestApiResponse.error(e)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): RestApiErrorResponse {
+        return RestApiResponse.error(Exception("올바르지 않은 요청입니다."))
     }
 }
